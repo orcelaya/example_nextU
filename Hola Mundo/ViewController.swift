c20c008fd6b7d01cf7aa11cad12f971794347d62
RC's MacBook Pro <RC@OMARs-MacBook-Pro.local>
2017-02-13 18:13:10 -0600

    Initial Commit

---
+++ Hola Mundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola Mundo
+//
+//  Created by OMAR RICARDO CELAYA T. on 2/13/17.
+//  Copyright © 2017 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
